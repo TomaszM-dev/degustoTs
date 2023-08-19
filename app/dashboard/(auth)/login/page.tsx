309d9ed830b6965d78f328bb659ad41dfbbe85f7
TomaszM-dev <tomekmalocha@gmail.com>
2023-08-19 17:17:54 +0200

    login & register ui

--- app/dashboard/(auth)/login/page.tsx
+++ app/dashboard/(auth)/login/page.tsx
@@ -7,6 +7,7 @@ import { Router } from "next/router";
 import LoadingSpinner from "@/app/components/LoadingSpinner";
 import { SignInResponse } from "next-auth/react";
 import { motion } from "framer-motion";
+import LoginAnimation from "@/app/components/LoginAnimation";
 
 const Login = () => {
   type response = {
@@ -46,40 +47,67 @@ const Login = () => {
   return (
     <>
       {session.status === "unauthenticated" && (
-        <div className="flex flex-col items-center border-[4px] border-white max-w-fit p-4 mx-auto rounded-lg mt-60">
-          <form
-            className="flex flex-col  py-10 px-4 gap-8 w-96 text-black"
-            onSubmit={handleSubmit}
-          >
-            <input
-              type="text"
-              className="p-5 bg-white w-full"
-              placeholder="email"
-              required
-            />
-            <input
-              type="password"
-              className="p-5 bg-white w-full"
-              placeholder="password"
-              required
-            />
-            <button className="px-6 py-4  font-[700] bg-main uppercase text-white">
-              Login
-            </button>
-          </form>
-          <div className="flex gap-10">
-            <button
-              onClick={() => {
-                signIn("google");
-                setIsLoading(true);
-              }}
-              className="border-b-2 pb-2"
-            >
-              Login With Google
-            </button>
-            <Link href="/dashboard/register " className="border-b-2 pb-2">
-              Register
-            </Link>
+        <div className="container h-screen px-6   ">
+          <div className=" flex h-full flex-wrap items-center justify-center lg:justify-between ">
+            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+              <LoginAnimation />
+            </div>
+
+            <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
+              <form onSubmit={handleSubmit}>
+                <div className="relative mb-4" data-te-input-wrapper-init>
+                  <input
+                    type="text"
+                    className=" w-full bg-main py-4 focus:outline-none shadow-sm shadow-purple  px-5"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+
+                <div className="relative mb-6" data-te-input-wrapper-init>
+                  <input
+                    type="password"
+                    className="  w-full bg-main py-4 focus:outline-none shadow-sm shadow-purple  px-5"
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+
+                <button
+                  className="btn gradientBg text-white w-full"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Sign in
+                </button>
+
+                <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                  <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
+                    OR
+                  </p>
+                </div>
+              </form>
+              <div className="flex flex-col gap-3 w-full">
+                <button
+                  onClick={() => {
+                    signIn("google");
+                    setIsLoading(true);
+                  }}
+                  className="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] "
+                  role="button"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Continue with Google
+                </button>
+                <Link
+                  href="/dashboard/register "
+                  className="w-full   px-3 py-3 text-center  uppercase btn text-white"
+                >
+                  Dont have account? Register Now
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       )}
@@ -87,11 +115,11 @@ const Login = () => {
       {err && (
         <motion.div
           onClick={() => setErr(false)}
-          className="absolute w-full h-full  left-0 z-10  flex items-center justify-center backdrop-blur-[0.2rem] "
+          className="absolute w-full h-full  left-0 z-10  flex items-center  justify-center backdrop-blur-[0.2rem] "
         >
           <motion.div
             onClick={(e) => e.stopPropagation()}
-            className="flex-col w-[30rem] h-[10rem] z-10 bg-black bg-opacity-100 flex shadow pt-4"
+            className="mb-40 flex-col w-[30rem] h-[10rem] z-10 bg-black bg-opacity-100 flex shadow pt-4"
           >
             <span
               onClick={() => setErr(false)}
